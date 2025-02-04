75912ed3e543bd868b0bbe2ccfc1bb1303bf146a
Karuna Shinde. <karunashinde22042003@gmail.com>
2025-02-04 11:52:22 +0530

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,6 @@
+﻿Build started at 01:36 PM...
+1>------ Build started: Project: ConsoleApp3, Configuration: Debug Any CPU ------
+1>Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
+1>ConsoleApp3 -> C:\Users\karuna shinde\source\repos\ConsoleApp3\ConsoleApp3\bin\Debug\net8.0\ConsoleApp3.dll
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 01:36 PM and took 00.283 seconds ==========
