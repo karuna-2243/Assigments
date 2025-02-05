4a44c7cdbe2e2243161c73ceb21ce40331645c42
Karuna Shinde. <karunashinde22042003@gmail.com>
2025-02-05 21:35:06 +0530

    Add files via upload

---
+++ que.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+        }
+        Console.Write("Enter your age:");
+        int age = int parse(Console.ReadLine());
+        Console.Write("Enter your profession");
+        string.profession = Console.ReadLine();
+        Console.Writeline($"\nHiee,{name)! You are {age} years old and work as {profession}.");
+
+    }
+}
