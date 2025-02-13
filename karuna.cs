721f377a4fbd0a2250b8e6f81f2aee107f9232a6
Karuna Shinde <karunashinde22042003@gmail.com>
2025-02-13 21:37:44 +0530

    Add files via upload

---
+++ karuna.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Msc CS!");
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            string abc = "Hello msc";
+            Console.WriteLine($"Hello, {name}! {abc}!!");
+        }
+    }
+}
